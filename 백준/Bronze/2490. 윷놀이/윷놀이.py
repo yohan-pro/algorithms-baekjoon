928456766f3e875f869b8b983b5376304dda5ac0
Yohan <yohanpro@icloud.com>
2025-07-27 01:07:56 +0900

    [Bronze III] Title: 윷놀이, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2490. 윷놀이/윷놀이.py
@@ -0,0 +1,7 @@
+yut = ["D", "C", "B", "A", "E"]
+a = map(int, input().split())
+b = map(int, input().split())
+c = map(int, input().split())
+print(yut[sum(a)])
+print(yut[sum(b)])
+print(yut[sum(c)])
